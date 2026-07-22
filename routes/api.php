e0709dbb88ec3d09e3212ad0a6f1d2440181836d
HafeezUllah886 <Hafeezullah886@gmail.com>
2026-07-23 00:43:55 +0500

    sd

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,16 @@
 <?php
 
 use App\Http\Controllers\api\authController;
-use App\Http\Controllers\api\nonFinanancialInfoController;
+use App\Http\Controllers\api\NonFinancialInfoController;
 use App\Http\Controllers\api\OrderbookerLocationsController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [authController::class, 'login']);
-Route::get('/products', [nonFinanancialInfoController::class, 'products']);
+Route::get('/products', [NonFinancialInfoController::class, 'products']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [authController::class, 'logout']);
 
-    Route::get('/units', [nonFinanancialInfoController::class, 'units']);
+    Route::get('/units', [NonFinancialInfoController::class, 'units']);
     Route::get('/storelocation', [OrderbookerLocationsController::class, 'store']);
 });
